77dd980470f46c645101d46db9c253e3c18fcab9
Tristan Cooper <Tristan.Cooper@Tristan.com>
2026-06-06 11:50:11 -0500

    Minor code updates, definition renaming, & comment updating. Updated description in 'platformio.ini

--- src/TDS3231.h
+++ src/TDS3231.h
@@ -3,7 +3,7 @@
   #include <TSoftwareVersion.h>
     #define TDS3231_V_MAJOR 1
     #define TDS3231_V_MINOR 1
-    #define TDS3231_V_PATCH 0
+    #define TDS3231_V_PATCH 1
     extern const TSoftwareVersion TDS3231_Version;
 
   #include <Adafruit_I2CDevice.h>
@@ -11,41 +11,28 @@
   #include <TTime.h>
 
 
-  #define DS3231_ADDRESS        0x68 // I2C address for DS3231
-  #define DS3231_TIME           0x00 // Time register
-  #define DS3231_ALARM1         0x07 // Alarm 1 register
-  #define DS3231_ALARM2         0x0B // Alarm 2 register
-  #define DS3231_CONTROL        0x0E // Control register
-  #define DS3231_STATUSREG      0x0F // Status register
-  #define DS3231_TEMPERATUREREG 0x11 // Temperature register (high byte - low byte is at 0x12), 10-bit temperature value
+  #define DS3231_ADDRESS 0x68
+  #define REG_TIME       0x00
+  #define REG_CONTROL    0x0E
+  #define REG_STATUS     0x0F
+  #define REG_TEMP       0x11 // 0x12 = lower byte, 10-bit value
 
   inline uint8_t ttdowToDs3231(uint8_t d);
 
 
-
-  enum Ds3231SqwPinMode {
-    DS3231_OFF = 0x1C,            /**< Off */
-    DS3231_SquareWave1Hz = 0x00,  /**<  1Hz square wave */
-    DS3231_SquareWave1kHz = 0x08, /**<  1kHz square wave */
-    DS3231_SquareWave4kHz = 0x10, /**<  4kHz square wave */
-    DS3231_SquareWave8kHz = 0x18  /**<  8kHz square wave */
-  };
-
   class TDS3231 {
     protected:
-      static uint8_t bcd2bin(uint8_t val) { return val - 6 * (val >> 4); }
-      static uint8_t bin2bcd(uint8_t val) { return val + 6 * (val / 10); }
-      Adafruit_I2CDevice *i2c_dev = NULL; // Pointer to I2C bus interface
+      static uint8_t _bcd2bin(uint8_t val) { return val - 6 * (val >> 4); }
+      static uint8_t _bin2bcd(uint8_t val) { return val + 6 * (val / 10); }
+
+      Adafruit_I2CDevice *i2c_dev = NULL;
       uint8_t _readregister(uint8_t reg);
-      void _writeregister(uint8_t reg, uint8_t val);
+      void    _writeregister(uint8_t reg, uint8_t val);
       
     public:
       bool begin(TwoWire *wireInstance = &Wire);
-      
-      Ds3231SqwPinMode readSqwPinMode();
-      void writeSqwPinMode(Ds3231SqwPinMode mode);
 
-      bool   lostPower(void);
+      bool lostPower();
 
       void   set(const TTime &tt);
       TTime  getNow();
